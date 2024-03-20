f9266ffcb92646bb2f8d1faf2564cb8010442876
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-03-20 12:45:27 +0530

    connected ws

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,52 +1,32 @@
-const sendMessageToUser = async (link, message) => {
-  console.log("MESSAGE TO : ", link);
-  chrome.storage.local.set({ agentMessage: message });
-  const messageTabId = await new Promise<number>((resolve) => {
-    chrome.tabs.create(
-      {
-        url: link,
-        active: true,
-      },
-      (tab) => resolve(tab.id)
-    );
-  });
-
-  chrome.tabs.onUpdated.addListener(function tabUpdateListener(
-    tabId,
-    changeInfo
-  ) {
-    if (tabId === messageTabId && changeInfo.status === "complete") {
-      chrome.tabs.onUpdated.removeListener(tabUpdateListener);
-
-      setTimeout(() => {
-        chrome.tabs.remove(messageTabId, () => {
-          console.log("Tab closed.");
-        });
-      }, 10000);
-    }
-  });
-};
-
-const openOptionsPage = async () => {
-  const createdTabId = await new Promise<number>((resolve) => {
-    chrome.tabs.create(
-      {
-        url: chrome.runtime.getURL("options.html"),
-        active: true,
-      },
-      (tab) => resolve(tab.id)
-    );
-  });
-
-  console.log("HERE IS OPTIONS PAGE : ", createdTabId);
-};
+const socket = new WebSocket("ws://localhost:8080");
+const tasks = [];
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "OPEN_OPTIONPAGE") {
-    openOptionsPage();
-  } else if (request.action === "SEND_MESSAGE") {
-    const link = `https://mbasic.facebook.com/profile.php?id=${request.user}&eav=AfaOZtP06KtwcohUXMijI7CJmht29uZfQ8kOo4tdX4BnpCTgOb693S77xj6E3dlfv90&paipv=0`;
-    const message = request.message;
-    sendMessageToUser(link, message);
+  const data = request.data;
+  socket.send(JSON.stringify({ action: "sendMessage", payload: data }));
+  sendResponse({ status: "ok" });
+});
+
+socket.addEventListener("open", (e) => {
+  console.log("Connection opened");
+});
+
+socket.addEventListener("message", (e) => {
+  const data = JSON.parse(e.data);
+  const message = data.message;
+
+  if (message === "connected") {
+    console.log("CONNECTED");
+  }
+
+  if (message === "taskAdded") {
+    console.log("TASK ADDED");
+  }
+
+  if (message === "sendMessageToUser") {
+    const task = data.task;
+    tasks.push(task);
+
+    console.log("Pending Tasks : ", tasks);
   }
 });
---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="dash">
+      <div className="container">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
