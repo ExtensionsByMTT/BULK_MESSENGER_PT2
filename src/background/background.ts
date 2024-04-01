f635a13f7511aef6a39cbf83ba8f461402b96c45
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-04-01 18:47:47 +0530

    working on reconnect error

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,47 +1,8 @@
 // const SOCKET_SERVER_URL = "ws://localhost:3001";
 // const SERVER_URL = "http://localhost:3001";
-
-let webSocket = null;
-
-function connects() {
-  webSocket = new WebSocket("wss://fbm.expertadblocker.com");
-
-  webSocket.onopen = (event) => {
-    console.log("websocket open");
-    setTimeout(() => {
-      if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-        console.log("Closing websocket connection after 5 seconds.");
-        webSocket.close();
-      }
-    }, 5000);
-    // keepAlive();
-  };
-
-  webSocket.onclose = (event) => {
-    console.log("websocket connection closed");
-    webSocket = null;
-  };
-}
-
-function keepAlive() {
-  const keepAliveIntervalId = setInterval(() => {
-    if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-      webSocket.send("keepalive");
-    } else {
-      clearInterval(keepAliveIntervalId);
-    }
-  }, 20 * 1000);
-}
-
-connects();
-
-const SOCKET_SERVER_URL = "wss://fbm.expertadblocker.com";
 const SERVER_URL = "https://fbm.expertadblocker.com";
-
-let socket: WebSocket | undefined;
-let reconnectAttempt = 0;
-const maxReconnectAttempts = 10;
-const maxReconnectDelay = 30000;
+const webSocket = new WebSocket("wss://fbm.expertadblocker.com");
+let reconnectInterval = 5000;
 let client_id = "";
 let pendingTasks = null;
 
@@ -51,7 +12,69 @@ function generateUniqueId() {
   const uniqueId = randomStr + "-" + timestamp;
   return uniqueId;
 }
+//
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const data = request.data;
+  if (request.type === "addTask") {
+    if (webSocket.readyState === WebSocket.OPEN) {
+      webSocket.send(JSON.stringify({ action: "addTask", payload: data }));
+    } else {
+      console.log(
+        "WebSocket is not open. Current state:",
+        webSocket.readyState
+      );
+    }
+    sendResponse({ status: "ok", message: "Data sent to server" });
+  }
+});
+//
+function sendClientData(clientID) {
+  chrome.storage.local.get("token", (result) => {
+    if (result?.token === undefined) {
+      console.log("Token Not found");
+    } else {
+      setTimeout(() => {
+        if (webSocket && webSocket.readyState === WebSocket.OPEN) {
+          webSocket.send(
+            JSON.stringify({
+              action: "clientID",
+              payload: clientID,
+              token: result.token,
+            })
+          );
+        }
+      }, 1000);
+    }
+  });
+}
 
+//
+async function updateTask(messageId, updatedData) {
+  try {
+    // Construct the URL with the task ID
+    const url = `${SERVER_URL}/api/messages/${messageId}`;
+
+    // Send the PUT request and wait for the response
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    // Parse the response body as JSON
+    const data = await response.json();
+    console.log("Task updated successfully:", data);
+  } catch (error) {
+    console.error("There was a problem updating the task:", error);
+  }
+}
+//
 const searchUser = (user: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     chrome.tabs.create(
@@ -96,6 +119,7 @@ const searchUser = (user: string): Promise<string> => {
   });
 };
 
+//
 const sendMessage = (
   id: number,
   url: string,
@@ -151,134 +175,89 @@ const sendMessage = (
     });
   });
 };
-
-async function updateTask(messageId, updatedData) {
-  try {
-    // Construct the URL with the task ID
-    const url = `${SERVER_URL}/api/messages/${messageId}`;
-
-    // Send the PUT request and wait for the response
-    const response = await fetch(url, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedData),
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    // Parse the response body as JSON
-    const data = await response.json();
-    console.log("Task updated successfully:", data);
-  } catch (error) {
-    console.error("There was a problem updating the task:", error);
-  }
-}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const data = request.data;
-  if (request.type === "addTask") {
-    if (socket.readyState === WebSocket.OPEN) {
-      socket.send(JSON.stringify({ action: "addTask", payload: data }));
-    } else {
-      console.log("WebSocket is not open. Current state:", socket.readyState);
-    }
-    sendResponse({ status: "ok", message: "Data sent to server" });
-  }
-});
-
-function sendClientData(clientID) {
-  chrome.storage.local.get("token", (result) => {
-    if (result?.token === undefined) {
-      console.log("Token Not found");
-    } else {
-      setTimeout(() => {
-        if (socket && socket.readyState === WebSocket.OPEN) {
-          socket.send(
-            JSON.stringify({
-              action: "clientID",
-              payload: clientID,
-              token: result.token,
-            })
-          );
-        }
-      }, 1000);
-    }
-  });
-}
-
-const connect = (clientID) => {
-  socket = new WebSocket(SOCKET_SERVER_URL);
-  console.log("Reconnect Attempt : ", reconnectAttempt);
-  console.log("Connected : ", clientID);
-
-  socket.addEventListener("close", () => {
-    console.log("Connection closed, attempting to reconnect...");
-    const delay = Math.min(1000 * 2 ** reconnectAttempt, maxReconnectDelay);
-    console.log(`Attempting to reconnect in ${delay / 1000} seconds...`);
-    chrome.alarms.create("reconnect", { delayInMinutes: delay / 60000 });
-    reconnectAttempt++;
-  });
-
-  socket.addEventListener("message", async (e) => {
+//
+const message = () => {
+  webSocket.onmessage = async (e) => {
     const data = JSON.parse(e.data);
 
-    if (data.action === "sendMessageToUser") {
-      console.log("TASK  : ", data);
-
-      const { id, sent_to, message } = data.task;
-      const chatURL = await searchUser(sent_to);
-
-      if (chatURL.length != 0) {
-        const result = await sendMessage(id, chatURL, message, sent_to);
-        await updateTask(id, result.res);
-      } else {
-        await updateTask(id, {
-          status: "failed",
-          message,
-          id,
-          user: sent_to,
-        });
-      }
+    if (data.action === "sendMessageToUser") console.log("TASK:", data);
+    const { id, sent_to, message } = data.task;
+    const chatURL = await searchUser(sent_to);
+    if (chatURL.length != 0) {
+      const result = await sendMessage(id, chatURL, message, sent_to);
+      await updateTask(id, result.res);
+    } else {
+      await updateTask(id, {
+        status: "failed",
+        message,
+        id,
+        user: sent_to,
+      });
     }
-
     if (data.action === "pendingTasks") {
       console.log("Your Pending Task : ", data.payload);
       chrome.storage.local.set({ pendingTasks: data.payload }, () => {
         console.log("Pending Task Saved to local Storage");
       });
     }
-
-    if (data.action === "timeLeft") {
-      console.log(`Time left: ${data.seconds} seconds`);
-    }
-  });
-
-  socket.addEventListener("open", async (event) => {
-    sendClientData(clientID);
-  });
+  };
 };
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === "reconnect") {
-    connect(client_id);
-  }
-});
-
-chrome.storage.local.get("clientID", (result) => {
-  if (result?.clientID === undefined) {
-    const newClientID = generateUniqueId();
-    chrome.storage.local.set({ clientID: newClientID }, () => {
-      console.log("Client ID generated and stored:", newClientID);
-      client_id = newClientID;
-      connect(newClientID);
+function connects() {
+  webSocket.onopen = () => {
+    console.log("WebSocket connected");
+    chrome.storage.local.get("clientID", (result) => {
+      if (result?.clientID === undefined) {
+        const newClientID = generateUniqueId();
+        chrome.storage.local.set({ clientID: newClientID }, () => {
+          console.log("Client ID generated and stored:", newClientID);
+          client_id = newClientID;
+          sendClientData(result.clientID);
+        });
+      } else {
+        console.log("Client ID already exists:", result.clientID);
+        client_id = result.clientID;
+        sendClientData(result.clientID);
+      }
     });
-  } else {
-    console.log("Client ID already exists:", result.clientID);
-    client_id = result.clientID;
-    connect(result.clientID);
+
+    keepAlive();
+    clearInterval(reconnectInterval);
+    /////////////////////////////////////////
+  };
+
+  //when webSocket get disconnect by any reason
+  webSocket.onclose = () => {
+    console.log("WebSocket connection closed");
+    reconnect();
+  };
+
+  webSocket.onerror = (error) => {
+    console.error("WebSocket error:", error);
+  };
+}
+
+function keepAlive() {
+  const keepAliveIntervalId = setInterval(() => {
+    if (webSocket) {
+      webSocket.send("keepalive");
+    } else {
+      clearInterval(keepAliveIntervalId);
+    }
+  }, 20000);
+}
+
+function reconnect() {
+  if (webSocket) {
+    webSocket.close();
   }
-});
+  console.log("Attempting to reconnect");
+  setTimeout(connects, reconnectInterval);
+
+  const maxReconnectInterval = 30000;
+  if (reconnectInterval > maxReconnectInterval) {
+    reconnectInterval = maxReconnectInterval;
+  }
+}
+
+connects();
