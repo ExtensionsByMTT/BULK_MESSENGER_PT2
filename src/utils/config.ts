a3ed5b551b21778055dc6f021b240213440ebbf9
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-06-15 12:10:49 +0530

    final build

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,11 +2,11 @@ const production = false;
 
 const _config = {
   SERVER_URL: production
-    ? "https://fbm.expertadblocker.com"
-    : "http://localhost:3001",
+    ? "https://bulk-messenger-pt-4.onrender.com"
+    : "http://localhost:5001",
   SOCKET_SERVER_URL: production
-    ? "wss://fbm.expertadblocker.com"
-    : "ws://localhost:3001",
+    ? "wss://bulk-messenger-pt-4.onrender.com"
+    : "ws://localhost:5001",
 };
 
 export const config = Object.freeze(_config);
