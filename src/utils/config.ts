a6bf2a6ec860c91e0488f71a80d884d97ac9406a
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-07-03 10:18:01 +0530

    final build for test

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,11 @@
-const production = false;
+const production = true;
 
 const _config = {
   SERVER_URL: production
-    ? "https://bulk-messenger-pt-4.onrender.com"
+    ? "https://bulk-messenger-pt-4-ek29.onrender.com"
     : "http://localhost:5001",
   SOCKET_SERVER_URL: production
-    ? "wss://bulk-messenger-pt-4.onrender.com"
+    ? "wss://bulk-messenger-pt-4-ek29.onrender.com"
     : "ws://localhost:5001",
 };
 
