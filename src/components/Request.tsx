eb7e253e7e4f0e3ab92c16a9b3916c846f89e9c6
Ashish <ashishsoni.underpinservices@gmail.com>
2025-04-29 11:34:40 +0530

    updated: values of count

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -155,16 +155,16 @@ const Request = () => {
               value={count}
               onChange={(e) => setCount(e.target.value)}
             >
-              <option value="2">1</option>
+              <option value="1">1</option>
               <option value="2">2</option>
-              <option value="2">3</option>
-              <option value="2">4</option>
-              <option value="2">5</option>
-              <option value="2">6</option>
-              <option value="3">7</option>
-              <option value="5">8</option>
-              <option value="5">9</option>
-              <option value="5">10</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
             </select>
           </div>
           <div className="btn">
